49a699c784738e80b4a8a263e7c1b4071df6d71d
Mihail Atanassov <atanassov.mihail@gmail.com>
2015-03-06 15:27:50 +0100

    Bug fix: Lucene
    Change wrong SortField.Type.INT to SortField.Type.STRING. The bug
    produces the following exception: java.lang.NumberFormatException:
    Invalid shift value in prefixCoded bytes (is encoded value really an
    INT?)

--- maalr.lucene/src/main/java/de/uni_koeln/spinfo/maalr/lucene/core/Dictionary.java
+++ maalr.lucene/src/main/java/de/uni_koeln/spinfo/maalr/lucene/core/Dictionary.java
@@ -252,7 +252,7 @@ public class Dictionary {
 			query = bc;
 			TopDocs docs = indexProvider.getSearcher().search(query,
 					null, pageSize,
-					new Sort(new SortField(sortField, SortField.Type.INT)));
+					new Sort(new SortField(sortField, SortField.Type.STRING)));
 
 			return toQueryResult(docs, 0, pageSize);
 		} catch (IOException e) {
